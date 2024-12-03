2bb2c04960d8bec09e71b73198330b4e6ff8509f
Ian Gathua Mbui <gathuambui@gmail.com>
2024-12-03 19:06:54 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetDistance){
+  if (feetDistance <= 400){
+    return ('This one is on me!')
+  } else if(feetDistance >400 && feetDistance <= 2000){
+      return ('That will be twenty bucks.')
+  } 
+    else if(feetDistance >2000 && feetDistance <= 2500){
+      return ('I will gladly take your thirty bucks.')
+  }
+    else if(feetDistance >2500){
+      return ('No can do.')
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  if (cityName === 'NYC'){
+    return ('Ok, sounds good.')
+  } else{
+    return ('No go.')
+  }
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous'){
+    return ("Thank you so much.")
+  }
+  else if(tip === 'not as generous'){
+    return ('Thank you.')
+  } else{
+    return ('Bye.')
+  }
 }
